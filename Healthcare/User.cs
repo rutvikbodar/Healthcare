49fc38462706d4a420a87fc6ea3a81df8fa4b2eb
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-10-12 14:23:07 +0530

    cache1

---
+++ Healthcare/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Healthcare
+{
+    public class User
+    {
+        public string username { get; set; }
+        public int password { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+    }
+}
