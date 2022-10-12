cf6927df34d39dd9d4c4e3f01b4642af677a0b49
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-10-12 12:36:32 +0530

    initial setup

---
+++ Healthcare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Healthcare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
